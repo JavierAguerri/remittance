e1de8e85062b5ead3607c36ae56c2250a74f1c3a
JavierAguerri <javieraguerrimoreno@gmail.com>
2019-07-23 23:27:10 +0200

    a different way

--- contracts/Remittance.sol
+++ contracts/Remittance.sol
@@ -8,7 +8,6 @@ contract Remittance is Pausable {
     using SafeMath for uint256;
 
     mapping (bytes32 => uint) public funds;
-    mapping (bytes32 => address) public authAddresses;
 
     event LogFundsAdded (
         address indexed sender,
@@ -16,51 +15,36 @@ contract Remittance is Pausable {
         bytes32 indexed hashedHashes
     );
 
-    event LogAddressAdded (
-        address indexed sender,
-        bytes32 indexed signature
-    );
-
     event LogFundsReleased (
         address indexed sender,
         uint value
     );
 
-    function addFunds(bytes32 hashpass1, bytes32 hashpass2) payable public whenNotPaused {
-        require(hashpass1!=keccak256(abi.encodePacked("")), "Password 1 cannot be empty");
-        require(hashpass2!=keccak256(abi.encodePacked("")), "Password 2 cannot be empty");
+    event LogHash (
+        bytes32 indexed passHash
+    );
+
+    function addFunds(bytes32 passHash) payable public whenNotPaused {
         require(msg.value>0, "Amount stored cannot be zero");
-        bytes32 hashedHashes = keccak256(abi.encodePacked(hashpass1, hashpass2));
-        funds[hashedHashes] = funds[hashedHashes].add(msg.value);
-        emit LogFundsAdded(msg.sender, msg.value, hashedHashes);
+        funds[passHash] = funds[passHash].add(msg.value);
+        emit LogFundsAdded(msg.sender, msg.value, passHash);
     }
 
-    // Before claiming the funds, the exchange shop submits a signature made of pass1, pass2 and the claiming address
-    // This will prevent an attacker from listening the network to submit a faster "releaseFunds" and steal the funds
-    // once the releaseFunds method is called by the legit user, the attacker could take the passwords and launch his own
-    // addAddress+releaseFunds, but since those are 2 methods to execute the chances are slimmer.
-
-    function addAddress(bytes32 signature) public whenNotPaused {
-        authAddresses[signature] = msg.sender;
-        emit LogAddressAdded(msg.sender, signature);
+    function testHash (string memory input1, string memory input2) public whenNotPaused {
+        bytes32 passHash = hash2psswds(input1,input2);
+        emit LogHash(passHash);
     }
 
-    // why inputSignature as a separate parameter? Because for some reason keccak256 function yields a different result in Solidity and in truffle
-    // when inputting multiple parameters (it works fine with only 1 parameter). The workaround that I found was to pass an already joined string
-    // to solidity and pass it to keccak256. Suggestions and explanations are very welcome!!
+    function hash2psswds(string memory input1, string memory input2) private view whenNotPaused returns (bytes32 passHash){
+        passHash = keccak256(abi.encodePacked(input1,input2));
+    }
 
-    function releaseFunds(string memory pass1, string memory pass2, string memory inputSignature) public whenNotPaused {
-        bytes32 hashpass1 = keccak256(abi.encodePacked(pass1));
-        bytes32 hashpass2 = keccak256(abi.encodePacked(pass2));
-        bytes32 hashedHashes = keccak256(abi.encodePacked(hashpass1, hashpass2));
-        uint amount = funds[hashedHashes];
+    function releaseFunds(string memory pass1, string memory pass2) public whenNotPaused {
+        bytes32 passHash = hash2psswds(pass1,pass2);
+        uint amount = funds[passHash];
         require(amount > 0, "No funds available");
-        bytes32 signature = keccak256(abi.encodePacked(inputSignature));
-        address receiver = authAddresses[signature];
-        require(authAddresses[signature] == msg.sender, "Wrong address");
-        funds[hashedHashes] = 0;
+        funds[passHash] = 0;
         emit LogFundsReleased(msg.sender, amount);
-        authAddresses[signature] = address(0);
         msg.sender.transfer(amount);
     }
 }
--- test/remittance.js
+++ test/remittance.js
@@ -2,7 +2,6 @@ const Remittance = artifacts.require("Remittance");
 const chai = require('chai');
 const BN = web3.utils.BN;
 const truffleAssert = require('truffle-assertions');
-const keccak256 = require('keccak256');
 chai.use(require('chai-bn')(BN));
 
 contract('Remittance', (accounts) => {
@@ -10,22 +9,22 @@ contract('Remittance', (accounts) => {
   const accountOne = accounts[0];
   const accountTwo = accounts[1];
   const accountThree = accounts[2];
-  const pass1 = "123456";
-  const pass2 = "456789";
-  const hashpass1 = keccak256(pass1);
-  const hashpass2 = keccak256(pass2);
-  const inputSignature1 = pass1+pass2+accountOne;
-  const signature1 = keccak256(inputSignature1);
-  const inputSignature2 = pass1+pass2+accountThree;
-  const signature2 = keccak256(inputSignature2);
-  //console.log("HashedHash: ", keccak256(hashpass1,hashpass2));
+
+  // If the pass strings are only made up of numbers, the tests will not be valid. I think it is related to the fact that
+  // toHex function treats strings made of numbers as a number, but I do not know how to force to treat it as UTF8 encoding
+  const pass1 = "123456b";
+  const pass2 = "456789a";
+  const pass1Hex = web3.utils.toHex(pass1);
+  const pass2Hex = web3.utils.toHex(pass2);
+  const passHex = web3.utils.toHex(pass1+pass2);
+  console.log(pass1Hex);
+  console.log(pass2Hex);
+  console.log(passHex);
+
+  const passHash = web3.utils.soliditySha3(passHex);
+  console.log("HashedHash: ", passHash);
   const pass3 = "147852";
   const pass4 = "369852";
-  const hashpass3 = keccak256(pass3);
-  const hashpass4 = keccak256(pass4);
-  const signature3 = keccak256(pass3,pass4,accountTwo);
-  const emptyhash = keccak256("");
-  //console.log(emptyhash);
   const accountZero = "0x0000000000000000000000000000000000000000";
   const amountBN = new BN('1000000000000',10);
   //console.log("Amount is: "+amountBN);
@@ -35,139 +34,28 @@ contract('Remittance', (accounts) => {
   });
 
   it('should fail when releasing funds with the wrong passwords', async () => {
-    await remittanceInstance.addFunds(hashpass1, hashpass2, {from: accountOne, value: amountBN});
-    await remittanceInstance.addAddress(signature1, {from: accountOne});
+    await remittanceInstance.addFunds(passHash, {from: accountOne, value: amountBN});
+    console.log("test 1: ",passHash);
+    await remittanceInstance.testHash(pass1, pass2, {from: accountThree});
     await truffleAssert.fails(
-      remittanceInstance.releaseFunds(pass3, pass4, inputSignature1, {from: accountOne}),
+      remittanceInstance.releaseFunds(pass3, pass4, {from: accountOne}),
       truffleAssert.ErrorType.REVERT
     );
   });
 
-  it('should fail when releasing funds with the right passwords, using another account', async () => {
-    await remittanceInstance.addFunds(hashpass1, hashpass2, {from: accountOne, value: amountBN});
-    await remittanceInstance.addAddress(signature1, {from: accountOne});
-    //console.log("Signature1: ",signature1);
-    await truffleAssert.fails(
-      remittanceInstance.releaseFunds(pass1, pass2, inputSignature1, {from: accountThree}),
-      truffleAssert.ErrorType.REVERT
-    );
+  it('should work when releasing funds with the right passwords, using another account', async () => {
+    await remittanceInstance.addFunds(passHash, {from: accountOne, value: amountBN});
+    console.log("test 2: ",passHash);
+    await remittanceInstance.testHash(pass1, pass2, {from: accountThree});
+    await remittanceInstance.releaseFunds(pass1, pass2, {from: accountThree});
   });
 
   it('should work when releasing funds with the right passwords, using the same account', async () => {
-    await remittanceInstance.addFunds(hashpass1, hashpass2, {from: accountOne, value: amountBN});
-    await remittanceInstance.addAddress(signature1, {from: accountOne});
-    //console.log("Signature1: ",signature1);
-    await remittanceInstance.releaseFunds(pass1, pass2, inputSignature1, {from: accountOne});
+    await remittanceInstance.addFunds(passHash, {from: accountOne, value: amountBN});
+    console.log("test 3: ",passHash);
+    await remittanceInstance.testHash(pass1, pass2, {from: accountThree});
+    await remittanceInstance.releaseFunds(pass1, pass2, {from: accountOne});
   });
 
-  it('should work when releasing the funds with the right passwords - check balances', async () => {
-
-    const initialBalanceOne = await web3.eth.getBalance(accountOne);
-    const initialBalanceOneBN = new BN (initialBalanceOne,10);
-
-    const receipt0 = await remittanceInstance.addFunds(hashpass1, hashpass2, {from: accountOne, value: amountBN});
-    const gasUsed0BN = new BN (receipt0.receipt.gasUsed,10);
-    const tx0 = await web3.eth.getTransaction(receipt0.tx);
-    const gasPrice0BN = new BN (tx0.gasPrice,10);
-
-    const step1BalanceOne = await web3.eth.getBalance(accountOne);
-    const step1BalanceOneBN = new BN (step1BalanceOne,10);
-
-    const calc1BalanceOneBN = initialBalanceOneBN.sub(gasUsed0BN.mul(gasPrice0BN)).sub(amountBN);
-    assert.strictEqual(calc1BalanceOneBN.toString(), step1BalanceOneBN.toString(), 'Account 1 balance does not match the expected - step 1');
-
-    const receipt1 = await remittanceInstance.addAddress(signature1, {from: accountOne});
-    const gasUsed1BN = new BN (receipt1.receipt.gasUsed,10);
-    const tx1 = await web3.eth.getTransaction(receipt1.tx);
-    const gasPrice1BN = new BN (tx1.gasPrice,10);
-
-    const step2BalanceOne = await web3.eth.getBalance(accountOne);
-    const step2BalanceOneBN = new BN (step2BalanceOne,10);
-
-    const calc2BalanceOneBN = step1BalanceOneBN.sub(gasUsed1BN.mul(gasPrice1BN));
-    assert.strictEqual(calc2BalanceOneBN.toString(), step2BalanceOneBN.toString(), 'Account 1 balance does not match the expected - step 2');
-
-    const receipt2 = await remittanceInstance.releaseFunds(pass1, pass2, inputSignature1, {from: accountOne});
-    const gasUsed2BN = new BN (receipt2.receipt.gasUsed,10);
-    const tx2 = await web3.eth.getTransaction(receipt2.tx);
-    const gasPrice2BN = new BN (tx2.gasPrice,10);
-
-    const step3BalanceOne = await web3.eth.getBalance(accountOne);
-    const step3BalanceOneBN = new BN (step3BalanceOne,10);
-
-    const calc3BalanceOneBN = step2BalanceOneBN.sub(gasUsed2BN.mul(gasPrice2BN)).add(amountBN);
-    assert.strictEqual(calc3BalanceOneBN.toString(), step3BalanceOneBN.toString(), 'Account 1 balance does not match the expected - step 3');
-
-  });
-
-  it('should fail when sending 0 Ether', async () => {
-    await truffleAssert.fails(
-      remittanceInstance.addFunds(hashpass1, hashpass2, {from: accountOne, value: 0}),
-      truffleAssert.ErrorType.REVERT
-    );
-  });  
-
-  it('should fail when sending 0 Ether', async () => {
-    await truffleAssert.fails(
-      remittanceInstance.addFunds(hashpass1, hashpass2, {from: accountOne, value: 0}),
-      truffleAssert.ErrorType.REVERT
-    );
-  });
-
-  it('should fail when sending an empty password 1', async () => {
-    await truffleAssert.fails(
-      remittanceInstance.addFunds(emptyhash, hashpass2, {from: accountOne, value: amountBN}),
-      truffleAssert.ErrorType.REVERT
-    );  
-  });
-
-  it('should fail when sending an empty password 2', async () => {
-    await truffleAssert.fails(
-      remittanceInstance.addFunds(hashpass1, emptyhash, {from: accountOne, value: amountBN}),
-      truffleAssert.ErrorType.REVERT
-    );  
-  });
-
-  it('should fail executing addFunds when paused', async () => {
-    await remittanceInstance.pause({from: accountOne});
-    await truffleAssert.fails(
-      remittanceInstance.addFunds(hashpass1, hashpass2, {from: accountOne, value: amountBN}),
-      truffleAssert.ErrorType.REVERT
-    );
-  });
-
-  it('should fail executing releaseFunds when paused', async () => {
-    await remittanceInstance.addFunds(hashpass1, hashpass2, {from: accountOne, value: amountBN});
-    await remittanceInstance.pause({from: accountOne});
-    await truffleAssert.fails(
-      remittanceInstance.releaseFunds(pass1, pass2, inputSignature1, {from: accountOne, value: amountBN}),
-      truffleAssert.ErrorType.REVERT
-    );
-  });
-
-  it('should be ok when pausing, executing, failing, unpausing and executing again', async () => {
-    await remittanceInstance.pause({from: accountOne});
-    await truffleAssert.fails(
-      remittanceInstance.addFunds(hashpass1, hashpass2, {from: accountOne, value: amountBN}),
-      truffleAssert.ErrorType.REVERT
-    );
-    await remittanceInstance.unpause({from: accountOne});
-    await remittanceInstance.addFunds(hashpass1, hashpass2, {from: accountOne, value: amountBN});
-  });
-
-  it('should fail when paused by not owner', async () => {
-    await truffleAssert.fails(
-      remittanceInstance.pause({from: accountTwo}),
-      truffleAssert.ErrorType.REVERT
-    );
-  });
-
-  it('should fail when unpaused by not owner', async () => {
-    await remittanceInstance.pause({from: accountOne});
-    await truffleAssert.fails(
-      remittanceInstance.unpause({from: accountTwo}),
-      truffleAssert.ErrorType.REVERT
-    );
-  });
 
 });
